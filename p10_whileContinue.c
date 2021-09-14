69e0d2be325a82ae0816712772aa7e8de49dd3e9
성우 <65459616+ABizCho@users.noreply.github.com>
2021-09-14 20:08:39 +0000

    p9_whileBreak.c p10.whileContinue.c
    Practice

---
+++ p10_whileContinue.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 0;
+
+  while ( i < 10 )
+  {
+    printf("i : %d\n",i);
+    i++;
+    printf("i++ : %d\n\n",i);
+
+    if ( i == 7)
+    {
+      printf("i==7 -> continue\n\n");
+      continue;
+    }
+  }
+  printf("end");
+  return 0;
+}
