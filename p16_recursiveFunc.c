c88adece4ecc0b2986f65e6171a8dbf1d78e76cb
성우 <65459616+ABizCho@users.noreply.github.com>
2021-09-19 09:09:56 +0000

    Update p16_recursiveFunc.c

--- p16_recursiveFunc.c
+++ p16_recursiveFunc.c
@@ -1,18 +1,21 @@
-//재귀함수란 함수에 매개변수로 입력받은 인자를 해당 함수의 명령문 내에서 '매개변수명'으로 호출하여 활용하는 함수이다
-  //C에서는 매개변수명을 해당함수 내에서 호출하여 사용한다
-  //C++에서는 함수명(매개변수명)을 함수내에서 호출하여 사용한다.
-
+//재귀함수란 사용자정의함수내에서 스스로를 호출해서 사용하는 함수이다. if->return 을 사용해서 루프의 종료를 임의로 지정할 수 있다. 
 #include <stdio.h>
 
-int self_square(int n){
-  return n*n;
-}
+void milkJam(int count);
 
 int main()
 {
-  int result=0;
-  result = self_square(3);
-  printf("%d\n",result);
+  milkJam(1);
   return 0;
+
+}
+
+void milkJam(int count)
+{
+  printf("%s\n","milk");
+  if( count == 3 ) return;
   
+  milkJam(count+1);
+
+  printf("%s\n","jam");
 }
---
+++ prac.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  char* CHAR = "fall";
+  printf("%s",CHAR); 
+
+  return 0;
+}
